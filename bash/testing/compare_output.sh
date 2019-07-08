fb26594b6c008397c5079c006b3795bbdad2fa38
Comran Morshed <comran.morshed@dji.com>
2019-07-08 14:16:16 -0700

    Report test success.

--- bash/testing/compare_output.sh
+++ bash/testing/compare_output.sh
@@ -11,10 +11,12 @@ function compare_output {
 
   if [ "$ACTUAL_OUTPUT" != "$CORRECT_OUTPUT" ]
   then
-    printf "\e[31mError: Output mismatch in $TEST_PATH\e[0m\n"
+    printf "\e[91mTest failed (Output mismatch): $TEST_PATH\e[0m\n"
     printf "Expected: $CORRECT_OUTPUT\n"
     printf "Got: $ACTUAL_OUTPUT\n"
     exit 1
+  else
+    printf "\e[92mTest passed: $TEST_PATH\e[0m\n"
   fi
 }
 
